7c17886cf5ed1573e2aa974fa232edea750b34fa
nithinK-142 <nithin.sagar.359@gmail.com>
2024-04-28 11:21:08 +0530

    feat: icons.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { useMemeStore } from "@/stores/memeStore";
 import { onMounted } from "vue";
+import {
+  BookmarkIcon,
+  UserIcon,
+  HandThumbUpIcon,
+  TvIcon,
+} from "@heroicons/vue/24/solid";
 
 const memeStore = useMemeStore();
 
@@ -10,8 +16,8 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="container mx-auto px-4 py-8">
-    <h1 class="text-3xl font-semibold mb-6">Would You like a Meme?</h1>
+  <div class="container px-4 py-8 mx-auto">
+    <h1 class="mb-6 text-3xl font-semibold">Would You like a Meme?</h1>
 
     <div v-if="memeStore.isLoading">Loading...</div>
     <div v-else-if="memeStore.error" class="text-red-500">
@@ -19,17 +25,30 @@ onMounted(() => {
     </div>
     <div
       v-else
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+      class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
     >
       <div
         v-for="meme in memeStore.memes"
         :key="meme.postLink"
-        class="bg-stone-700/50 rounded-lg shadow-md p-4"
+        class="p-4 h-[30rem] rounded-lg shadow-md bg-stone-700/50"
       >
-        <h2 class="text-lg font-bold mb-2">{{ meme.title }}</h2>
-        <p class="mb-1">Subreddit: {{ meme.subreddit }}</p>
-        <p class="mb-1">Author: {{ meme.author }}</p>
-        <p class="mb-2">Upvotes: {{ meme.ups }}</p>
+        <h2 class="flex items-center mb-2 text-lg font-bold">
+          <BookmarkIcon class="w-5 h-5 mr-2 text-gray-400" />
+          {{ meme.title }}
+        </h2>
+        <div class="flex items-center mb-2">
+          <TvIcon class="w-4 h-4 mr-2 text-gray-400" />
+          {{ meme.subreddit }}
+        </div>
+        <div class="flex items-center mb-2">
+          <UserIcon class="w-4 h-4 mr-2 text-gray-400" />
+          {{ meme.author }}
+        </div>
+        <div class="flex items-center mb-2">
+          <HandThumbUpIcon class="w-4 h-4 mr-2 text-gray-400" />
+          {{ meme.ups }} Upvotes
+        </div>
+
         <img
           :src="meme.url"
           :alt="meme.title"
