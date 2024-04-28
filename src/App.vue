c20155b105728d8cb00fada1df16bd3bf88cbe02
nithinK-142 <nithin.sagar.359@gmail.com>
2024-04-28 09:58:59 +0530

    feat: memes.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,41 @@
+<script setup lang="ts">
+import { useMemeStore } from "@/stores/memeStore";
+import { onMounted } from "vue";
+
+const memeStore = useMemeStore();
+
+onMounted(() => {
+  memeStore.fetchMemes();
+});
+</script>
+
 <template>
-  <main>
-    <h1 class="text-4xl font-semibold text-center">Would You like a Meme?</h1>
-  </main>
+  <div class="container mx-auto px-4 py-8">
+    <h1 class="text-3xl font-semibold mb-6">Would You like a Meme?</h1>
+
+    <div v-if="memeStore.isLoading">Loading...</div>
+    <div v-else-if="memeStore.error" class="text-red-500">
+      {{ memeStore.error }}
+    </div>
+    <div
+      v-else
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+    >
+      <div
+        v-for="meme in memeStore.memes"
+        :key="meme.postLink"
+        class="bg-stone-700/50 rounded-lg shadow-md p-4"
+      >
+        <h2 class="text-lg font-bold mb-2">{{ meme.title }}</h2>
+        <p class="mb-1">Subreddit: {{ meme.subreddit }}</p>
+        <p class="mb-1">Author: {{ meme.author }}</p>
+        <p class="mb-2">Upvotes: {{ meme.ups }}</p>
+        <img
+          :src="meme.url"
+          :alt="meme.title"
+          class="w-full h-auto rounded-md"
+        />
+      </div>
+    </div>
+  </div>
 </template>
---
+++ src/stores/memeStore.ts
@@ -0,0 +1,38 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+
+interface Meme {
+  postLink: string;
+  subreddit: string;
+  title: string;
+  url: string;
+  nsfw: boolean;
+  spoiler: boolean;
+  author: string;
+  ups: number;
+  preview: string[];
+}
+
+export const useMemeStore = defineStore("memeStore", () => {
+  const MEME_API_URL = "https://meme-api.com/gimme/4";
+  const memes = ref<Meme[]>([]);
+  const isLoading = ref<boolean>(false);
+  const error = ref<string | null>(null);
+
+  const fetchMemes = async () => {
+    isLoading.value = true;
+    error.value = null;
+
+    try {
+      const response = await fetch(MEME_API_URL);
+      const data = await response.json();
+      memes.value = data.memes;
+    } catch (err: any) {
+      error.value = err.message;
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  return { memes, isLoading, error, fetchMemes };
+});
