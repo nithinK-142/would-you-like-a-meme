36c3f258745f190c2d247baabc3c7850511601b0
nithinK-142 <nithin.sagar.359@gmail.com>
2024-05-09 22:24:44 +0530

    feat: fetch more memes.

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -1,9 +1,20 @@
+<script setup lang="ts">
+import { useMemeStore } from "@/stores/memeStore";
+import { RefreshCwIcon } from "lucide-vue-next";
+
+const memeStore = useMemeStore();
+const fetchMemes = memeStore.fetchMemes;
+</script>
+
 <template>
   <header>
     <div class="fixed flex p-4 w-full bg-[#212121]">
       <h1 class="text-base font-semibold sm:text-lg md:text-xl">
         Would You like a Meme?
       </h1>
+      <button @click="fetchMemes" class="ml-4">
+        <RefreshCwIcon :class="memeStore.isLoading && 'animate-spin'" />
+      </button>
     </div>
   </header>
 </template>
