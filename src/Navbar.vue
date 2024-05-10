241a3648288fcd814ca5ec994e5642e6a884d7db
nithinK-142 <nithin.sagar.359@gmail.com>
2024-05-10 19:07:54 +0530

    fix: navbar position in small screens.

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -8,7 +8,7 @@ const fetchMemes = memeStore.fetchMemes;
 
 <template>
   <header>
-    <div class="fixed flex p-4 w-full bg-[#212121]">
+    <div class="fixed top-0 flex p-4 w-full bg-[#212121]">
       <h1 class="text-base font-semibold sm:text-lg md:text-xl">
         Would You like a Meme?
       </h1>
