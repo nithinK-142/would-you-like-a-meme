e93949a2ebbd199720a44522c3c23deadf9df753
nithinK-142 <nithin.sagar.359@gmail.com>
2024-05-11 17:09:24 +0530

    add: primevue presets.

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
 
+// @ts-ignore
+import Lara from "./presets/lara";
 import App from "./App.vue";
 
 const app = createApp(App);
 
 app.use(createPinia());
 
-app.use(PrimeVue, { unstyled: true });
+app.use(PrimeVue, { unstyled: true, pt: Lara });
 
 app.mount("#app");
