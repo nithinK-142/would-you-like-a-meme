fa79fdbac20f5319946c1b1a17bbf2d9c431f108
nithinK-142 <nithin.sagar.359@gmail.com>
2024-05-11 17:07:09 +0530

    add: primevue.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import "./main.css";
+import "primevue/resources/themes/aura-light-green/theme.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+
 import App from "./App.vue";
 
 const app = createApp(App);
 
 app.use(createPinia());
+
+app.use(PrimeVue, { unstyled: true });
 
 app.mount("#app");
