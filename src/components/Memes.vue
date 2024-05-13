ef33d2417f077b6f4551ebee5895d6537a0d179b
nithinK-142 <nithin.sagar.359@gmail.com>
2024-05-13 21:47:38 +0530

    add: primevue card.

--- src/components/Memes.vue
+++ src/components/Memes.vue
@@ -3,6 +3,7 @@ import { ArrowBigUp } from "lucide-vue-next";
 import reddit from "@/assets/reddit.svg";
 import redditUser from "@/assets/reddit-user.svg";
 import type { Meme } from "@/stores/memeStore";
+import Card from "primevue/card";
 
 defineProps<{ memes: Meme[] }>();
 </script>
@@ -10,49 +11,58 @@ defineProps<{ memes: Meme[] }>();
   <div
     class="grid grid-cols-1 gap-4 px-4 mt-16 mb-4 sm:grid-cols-2 lg:grid-cols-3 3xl:grid-cols-4"
   >
-    <div
+    <Card
+      class="p-4 bg-stone-700/50"
       v-for="meme in memes"
       :key="meme.postLink"
-      class="p-4 rounded-lg shadow-md bg-stone-700/50"
     >
-      <div class="flex items-center mb-2">
-        <img :src="reddit" class="w-5 h-5 mr-2" />
-        <a
-          :href="'https://www.reddit.com/r/' + meme.subreddit"
-          target="_blank"
-          class="text-sm font-semibold"
-        >
-          {{ meme.subreddit }}
-        </a>
-      </div>
-      <div class="flex space-x-4">
-        <div class="flex items-center mb-2">
-          <img :src="redditUser" class="w-5 h-5 mr-2" />
-          <span class="text-base">u/{{ meme.author }}</span>
+      <template #header>
+        <div class="flex items-center mb-1">
+          <img :src="reddit" class="w-5 h-5 mr-2" />
+          <a
+            :href="'https://www.reddit.com/r/' + meme.subreddit"
+            target="_blank"
+            class="text-sm font-semibold"
+          >
+            {{ meme.subreddit }}
+          </a>
         </div>
+        <div class="flex space-x-4">
+          <div class="flex items-center">
+            <img :src="redditUser" class="w-5 h-5 mr-2" />
+            <span class="text-base">u/{{ meme.author }}</span>
+          </div>
 
-        <div class="flex items-center mb-2">
-          <ArrowBigUp />
-          {{ meme.ups.toLocaleString() }}
+          <div class="flex items-center">
+            <ArrowBigUp />
+            {{ meme.ups.toLocaleString() }}
+          </div>
         </div>
-      </div>
-      <h2
-        v-if="
-          meme.subreddit.toLocaleLowerCase() !== meme.title.toLocaleLowerCase()
-        "
-        class="flex items-center mb-2 text-lg"
-      >
-        {{ meme.title }}
-      </h2>
-      <div class="h-[20rem]">
-        <a :href="meme.url" target="_blank">
-          <img
-            :src="meme.url"
-            :alt="meme.title"
-            class="object-contain w-full h-full mx-auto rounded-md"
-          />
-        </a>
-      </div>
-    </div>
+      </template>
+
+      <template #title>
+        <h2
+          v-if="
+            meme.subreddit.toLocaleLowerCase() !==
+            meme.title.toLocaleLowerCase()
+          "
+          class="mb-0 text-base font-normal"
+        >
+          {{ meme.title }}
+        </h2>
+      </template>
+
+      <template #content>
+        <div class="h-[20rem]">
+          <a :href="meme.url" target="_blank">
+            <img
+              :src="meme.url"
+              :alt="meme.title"
+              class="object-contain w-full h-full mx-auto rounded-md"
+            />
+          </a>
+        </div>
+      </template>
+    </Card>
   </div>
 </template>
--- src/presets/lara/card/index.js
+++ src/presets/lara/card/index.js
@@ -6,12 +6,12 @@ export default {
             'shadow-md',
 
             //Color
-            'bg-surface-0 dark:bg-surface-900',
-            'text-surface-700 dark:text-surface-0'
+            // 'bg-surface-0 dark:bg-surface-900',
+            // 'text-surface-700 dark:text-surface-0'
         ]
     },
     body: {
-        class: 'p-5'
+        class: 'p-5 pb-0'
     },
     title: {
         class: 'text-2xl font-bold mb-2'
@@ -29,9 +29,9 @@ export default {
         ]
     },
     content: {
-        class: 'py-5' // Vertical padding.
+        // class: 'py-5' // Vertical padding.
     },
     footer: {
-        class: 'pt-5' // Top padding.
+        // class: 'pt-5' // Top padding.
     }
 };
