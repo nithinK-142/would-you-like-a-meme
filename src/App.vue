dd84218715711cb8936a492ad6912f001271b4eb
nithinK-142 <nithin.sagar.359@gmail.com>
2024-05-17 11:00:37 +0530

    update: PrimeVue v3.

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import Drawer from "./components/Drawer.vue";
   <main
     class="flex flex-col items-center justify-center h-screen p-6 space-y-6"
   >
-    <h1 class="text-2xl font-semibold text-center">PrimeVue v4</h1>
+    <h1 class="text-2xl font-semibold text-center">PrimeVue v3</h1>
     <Button label="Submit" icon="pi pi-check" iconPos="right" />
     <OverlayAll />
     <Drawer />
